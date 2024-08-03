e8e5cfdaceb44d8234c367e8f25d0d5861cd9d67
superkhool <superkhool69@gmail.com>
2024-08-03 23:55:02 +0600

    Forgot password feature

--- accounts/utils.py
+++ accounts/utils.py
@@ -1,3 +1,13 @@
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode , urlsafe_base64_decode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail  import EmailMessage
+from django.conf import settings
+
+from accounts.models import User
+
 
 def DetectUser(user):
     if user.role ==1:
@@ -9,3 +19,53 @@ def DetectUser(user):
     elif user.role is None and user.is_superadmin:
         redirectUrl = '/admin'
         return redirectUrl
+    
+    
+#Daynamic Way To send mail for reset Password as well as Active The User! 
+def send_verification_email(request,user,mail_subject,email_template):
+    current_site = get_current_site(request)
+    from_email = settings.DEFAULT_FROM_EMAIL
+    message = render_to_string(email_template,{
+        'user': user,
+        'domain': current_site,
+        'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+        'token' : default_token_generator.make_token(user),
+        
+    })
+    to_email = user.email
+    mail = EmailMessage(mail_subject, message,from_email,to=[to_email])
+    mail.send()
+    
+ 
+  
+   
+# This is None Dynamic Way
+'''
+def send_password_reset_email(request,user):
+    current_site = get_current_site(request)
+    mail_subject = "RESET YOUR PASSWORD "
+    from_email = settings.DEFAULT_FORM_EMAIL
+    message = render_to_string('accounts/emails/reset_password_email.html',{
+        'user': user,
+        'domain': current_site,
+        'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+        'token' : default_token_generator.make_token(user),
+        
+    })
+    to_email = user.email
+    mail = EmailMessage(mail_subject, message,from_email,to=[to_email])
+    mail.send()
+    
+'''
+
+
+# Decoding The User From The Email And If user Exist Returning The User To The 
+def check_valid_user(user,uidb64):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = User._default_manager.get(pk=uid)
+    except(TypeError,ValueError,OverflowError,User.DoesNotExist):
+        user = None
+        
+    return user,uid
+    
--- accounts/views.py
+++ accounts/views.py
@@ -4,23 +4,27 @@ from vendor.forms import VendorForm
 from .forms import UserForm
 from .models import User, UserProfile
 from django.contrib import messages , auth
-from .utils import DetectUser
+from .utils import DetectUser, send_verification_email ,check_valid_user
 from django.contrib.auth.decorators import login_required , user_passes_test
 from django.core.exceptions import PermissionDenied
+from django.utils.http import urlsafe_base64_decode
+from django.contrib.auth.tokens import default_token_generator
+
+
+
 # Create your views here.
 
 
 
 #Restrict Vendor User To Access Customer Dashboard
-
 def check_vendor_user(user):
     if user.role == 1:
         return True
     else:
         raise PermissionDenied
 
-#Restrict Customer  User To Access Vendor Dashboard
 
+#Restrict Customer  User To Access Vendor Dashboard
 def check_customer_user(user):
     if user.role == 2:
         return True
@@ -54,7 +58,11 @@ def registeruser(request):
             user = User.objects.create_user(first_name = first_name,last_name = last_name, username = username, email = email,password = password)
             user.role = User.CUSTOMER
             user.save()
-            messages.success(request," User Account Register Sucessfully ")
+            #Send Verification Email
+            mail_subject = "Please Active  Your Account"
+            email_template = 'accounts/emails/account_verification_email.html'
+            send_verification_email(request,user, mail_subject , email_template)
+            messages.success(request," User Account Register Sucessfully! Check Email For Activation!")
             
             
             return redirect('login')
@@ -91,6 +99,11 @@ def registerVendor(request):
             user_profile = UserProfile.objects.get(user=user)
             vendor.user_profile = user_profile
             vendor.save()
+            
+            #Send Verification Email
+            mail_subject = "Please Active  Your Account"
+            email_template = 'accounts/emails/account_verification_email.html'
+            send_verification_email(request,user, mail_subject , email_template)
             messages.success(request,"Your Account Register Successfully! Please Wait for The Approval! ")
             return redirect('registerVendor')
         else:
@@ -107,6 +120,40 @@ def registerVendor(request):
     }
     
     return render(request,'accounts/registerVendor.html',context)
+
+
+
+
+#Active The User By is_active status set to  True
+def activate(request ,uidb64,token):
+     
+    user = check_valid_user(request.user,uidb64) #{I made A Helper Fucntion For It : Utils }
+    # try:
+    #     uid =urlsafe_base64_decode(uidb64).decode() #Decodeing The User Primery Key! COMES FROM django.utils.http
+    #     user = User._default_manager.get(pk=uid) #after Decoding PK Getting USER From The User Model 
+    # except(TypeError,ValueError,OverflowError,User.DoesNotExist): #Checking If User Doseno't Exist What Happen!
+    #     user = None
+        
+        
+    if user is not None and default_token_generator.check_token(user,token): #Checking USER AND Also The TOken By Token Genaretor Cmoes form django.contrib.Auth.token
+        if user.role == 1:  
+            user.is_active = True
+            user.save()
+            messages.success(request,'Congratulation! Your Restaurant Status is Active! Please Wait For The Aproval!')
+            return redirect('myAccount')
+        elif user.role == 2:
+            user.is_active = True
+            user.save()
+            messages.success(request,'Congratulation! User Active Sucessfull.')
+            return redirect('myAccount')
+    else: 
+        messages.error(request,'User Validation Failed .')
+        return redirect('myAccount')
+    
+        
+
+
+
 
 
 def login(request):
@@ -141,7 +188,7 @@ def myAccount(request):
 
 
 @login_required(login_url='login')
-@user_passes_test(check_customer_user)
+@user_passes_test(check_customer_user) # user Passes Test Deafult Decoretor Help To veryfi user role if it's pass user can acess the Dashboard 
 def custDashboard(request):
     return render(request,'accounts/custDashboard.html')
 
@@ -152,3 +199,52 @@ def vendorDashboard(request):
     return render(request,'accounts/vendorDashboard.html')
 
 
+
+def forgot_password(request):
+    
+    if request.method == "POST":
+        email = request.POST['email']
+        if User.objects.filter(email=email).exists():
+            user = User.objects.get(email__exact = email)
+            
+            mail_subject = "Reset Your Password"
+            email_template ='accounts/emails/reset_password_email.html'
+            send_verification_email(request,user,mail_subject,email_template)
+            messages.success(request,"Password Reset email Has Send To Your email address")
+            return redirect('login')
+        else:
+            messages.success(request,"Email Dose Not Exist")
+            return redirect('forgot_password')  
+    
+    return render(request,'accounts/forgot_password.html')
+
+
+def reset_password_validate(request,uidb64,token):
+    user , uid =check_valid_user(request.user,uidb64)
+    if user is not None and default_token_generator.check_token(user,token):
+        request.session['uid'] = uid #Saving The User id to the request session So that We can get it This User_id on the Reset_Password.html  
+        messages.info(request,"Please Reset Your Passwrod")
+        return redirect('reset_password')
+    else:
+        messages.warning(request , 'This Link Has Expierd ')
+        return redirect('myAccount')
+    
+     
+
+
+def reset_password(request):
+    if request.method == "POST":
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+        if password == confirm_password:
+            uid = request.session.get('uid')#Now Getting The User_id From The reset_password_validate.html or Function in views
+            user = User.objects.get(pk=uid)
+            user.set_password(password)
+            user.is_active = True
+            user.save()
+            messages.success(request ," Password Reset Sucessfully!")
+            return redirect('login')
+        else:
+            messages.warning(request,'Password Dose Not Matched')
+            return redirect('reset_password')
+    return render(request,'accounts/reset_password.html')
